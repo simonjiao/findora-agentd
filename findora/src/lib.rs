29c5fa3236ad460c7ccf644eb59f71cd8c55832b
simonjiao <simon@findora.org>
2022-02-17 17:53:29 +0800

    refactor with clap

---
+++ findora/src/lib.rs
@@ -0,0 +1,203 @@
+use bip0039::{Count, Language, Mnemonic};
+use bip32::{DerivationPath, XPrv};
+use libsecp256k1::{PublicKey, SecretKey};
+use serde::{Deserialize, Serialize};
+use sha3::{Digest, Keccak256};
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::runtime::Runtime;
+use web3::{
+    transports::Http,
+    types::{
+        Address, BlockNumber, Bytes, Transaction, TransactionId, TransactionParameters, TransactionReceipt, H160, H256,
+        U256, U64,
+    },
+};
+
+const FRC20_ADDRESS: u64 = 0x1000;
+pub const BLOCK_TIME: u64 = 16;
+
+//const WEB3_SRV: &str = "http://127.0.0.1:8545";
+//const WEB3_SRV: &str = "http://18.236.205.22:8545";
+const WEB3_SRV: &str = "https://prod-testnet.prod.findora.org:8545";
+//const WEB3_SRV: &str = "https://dev-mainnetmock.dev.findora.org:8545";
+
+pub const ROOT_SK: &str = "b8836c243a1ff93a63b12384176f102345123050c9f3d3febbb82e3acd6dd1cb";
+pub const ROOT_ADDR: &str = "0xBb4a0755b740a55Bf18Ac4404628A1a6ae8B6F8F";
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct KeyPair {
+    pub address: String,
+    pub private: String,
+}
+
+pub fn one_eth_key() -> KeyPair {
+    let mnemonic = Mnemonic::generate_in(Language::English, Count::Words12);
+    let bs = mnemonic.to_seed("");
+    let ext = XPrv::derive_from_path(&bs, &DerivationPath::from_str("m/44'/60'/0'/0/0").unwrap()).unwrap();
+
+    let secret = SecretKey::parse_slice(&ext.to_bytes()).unwrap();
+    let public = PublicKey::from_secret_key(&secret);
+
+    let mut res = [0u8; 64];
+    res.copy_from_slice(&public.serialize()[1..65]);
+    let public = H160::from(H256::from_slice(Keccak256::digest(&res).as_slice()));
+
+    KeyPair {
+        address: eth_checksum::checksum(&format!("{:?}", public)),
+        private: hex::encode(secret.serialize()),
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct TransferMetrics {
+    pub from: Address,
+    pub to: Address,
+    pub amount: U256,
+    pub hash: Option<H256>, // Tx hash
+    pub status: u64,        // 1 - success, 0 - fail
+    pub wait: u64,          // seconds for waiting tx receipt
+}
+
+pub struct TestClient {
+    pub web3: Arc<web3::Web3<Http>>,
+    pub root_sk: secp256k1::SecretKey,
+    pub root_addr: Address,
+    rt: Runtime,
+}
+
+impl TestClient {
+    pub fn setup(url: Option<&str>, root_sk: Option<&str>, root_addr: Option<&str>) -> Self {
+        let transport = web3::transports::Http::new(url.unwrap_or(WEB3_SRV)).unwrap();
+        let web3 = Arc::new(web3::Web3::new(transport));
+        let root_sk = secp256k1::SecretKey::from_str(root_sk.unwrap_or(ROOT_SK)).unwrap();
+        let root_addr = Address::from_str(root_addr.unwrap_or(ROOT_ADDR)).unwrap();
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        Self {
+            web3,
+            root_sk,
+            root_addr,
+            rt,
+        }
+    }
+
+    pub fn chain_id(&self) -> Option<U256> {
+        self.rt.block_on(self.web3.eth().chain_id()).ok()
+    }
+
+    pub fn block_number(&self) -> Option<U64> {
+        self.rt.block_on(self.web3.eth().block_number()).ok()
+    }
+
+    pub fn gas_price(&self) -> Option<U256> {
+        self.rt.block_on(self.web3.eth().gas_price()).ok()
+    }
+
+    pub fn frc20_code(&self) -> Option<Bytes> {
+        self.rt
+            .block_on(self.web3.eth().code(H160::from_low_u64_be(FRC20_ADDRESS), None))
+            .ok()
+    }
+
+    #[allow(unused)]
+    pub fn transaction(&self, id: TransactionId) -> Option<Transaction> {
+        self.rt.block_on(self.web3.eth().transaction(id)).unwrap_or_default()
+    }
+
+    pub fn transaction_receipt(&self, hash: H256) -> Option<TransactionReceipt> {
+        self.rt
+            .block_on(self.web3.eth().transaction_receipt(hash))
+            .unwrap_or_default()
+    }
+
+    #[allow(unused)]
+    pub fn accounts(&self) -> Vec<Address> {
+        self.rt.block_on(self.web3.eth().accounts()).unwrap_or_default()
+    }
+
+    pub fn balance(&self, address: Address, number: Option<BlockNumber>) -> U256 {
+        self.rt
+            .block_on(self.web3.eth().balance(address, number))
+            .unwrap_or_default()
+    }
+
+    pub fn distribution(
+        &self,
+        source: Option<(secp256k1::SecretKey, Address)>,
+        accounts: &[&str],
+        amounts: &[U256],
+        block_time: Option<u64>,
+    ) -> web3::Result<(Vec<TransferMetrics>, u64)> {
+        let mut results = vec![];
+        let mut succeed = 0u64;
+        let mut idx = 1u64;
+        let total = accounts.len();
+        let source_address = source.unwrap_or((self.root_sk, self.root_addr)).1;
+        let source_sk = source.unwrap_or((self.root_sk, self.root_addr)).0;
+        let wait_time = block_time.unwrap_or(BLOCK_TIME) * 3 + 1;
+        accounts
+            .iter()
+            .zip(amounts)
+            .map(|(&account, &am)| {
+                let to = Some(Address::from_str(account).unwrap());
+                let tm = TransferMetrics {
+                    from: source_address,
+                    to: to.unwrap(),
+                    amount: am,
+                    ..Default::default()
+                };
+                let tp = TransactionParameters {
+                    to,
+                    value: am,
+                    ..Default::default()
+                };
+                (tp, tm)
+            })
+            // Sign the txs (can be done offline)
+            .for_each(|(tx_object, mut metric)| {
+                if let Ok(signed) = self
+                    .rt
+                    .block_on(self.web3.accounts().sign_transaction(tx_object, &source_sk))
+                {
+                    if let Ok(hash) = self
+                        .rt
+                        .block_on(self.web3.eth().send_raw_transaction(signed.raw_transaction))
+                    {
+                        metric.hash = Some(hash);
+                        let mut retry = wait_time;
+                        loop {
+                            if let Some(receipt) = self.transaction_receipt(hash) {
+                                if let Some(status) = receipt.status {
+                                    if status == U64::from(1u64) {
+                                        succeed += 1;
+                                        metric.status = 1;
+                                    }
+                                }
+                                metric.wait = wait_time + 1 - retry;
+                                break;
+                            } else {
+                                std::thread::sleep(Duration::from_secs(1));
+                                retry -= 1;
+                                if retry == 0 {
+                                    metric.wait = wait_time;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                println!("{}/{} {:?} {}", idx, total, metric.to, metric.status == 1);
+                idx += 1;
+                results.push(metric);
+            });
+
+        println!("Tx succeeded: {}/{}", succeed, total);
+
+        Ok((results, succeed))
+    }
+}
--- findora/src/main.rs
+++ findora/src/main.rs
@@ -1,207 +1,57 @@
-use bip0039::{Count, Language, Mnemonic};
-use bip32::{DerivationPath, XPrv};
-use libsecp256k1::{PublicKey, SecretKey};
-use serde::{Deserialize, Serialize};
-use sha3::{Digest, Keccak256};
+use clap::{Parser, Subcommand};
 use std::ops::Mul;
 use std::str::FromStr;
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
-use std::{env, thread};
-use tokio::runtime::Runtime;
-use web3::{
-    transports::Http,
-    types::{
-        Address, BlockNumber, Bytes, Transaction, TransactionId, TransactionParameters, TransactionReceipt, H160, H256,
-        U256, U64,
-    },
+use std::{
+    path::PathBuf,
+    sync::{Arc, Mutex},
+    thread,
 };
 
-const FRC20_ADDRESS: u64 = 0x1000;
-const BLOCK_TIME: u64 = 16;
+use feth::{one_eth_key, KeyPair, TestClient, TransferMetrics, BLOCK_TIME, ROOT_ADDR};
+use web3::types::Address;
 
-//const WEB3_SRV: &str = "http://127.0.0.1:8545";
-//const WEB3_SRV: &str = "http://18.236.205.22:8545";
-const WEB3_SRV: &str = "https://prod-testnet.prod.findora.org:8545";
-//const WEB3_SRV: &str = "https://dev-mainnetmock.dev.findora.org:8545";
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Cli {
+    /// The minimum parallelism
+    #[clap(long, default_value_t = 10)]
+    min_parallelism: u64,
 
-const ROOT_SK: &str = "b8836c243a1ff93a63b12384176f102345123050c9f3d3febbb82e3acd6dd1cb";
-const ROOT_ADDR: &str = "0xBb4a0755b740a55Bf18Ac4404628A1a6ae8B6F8F";
+    /// The maximum parallelism
+    #[clap(long, default_value_t = 2000)]
+    max_parallelism: u64,
 
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
-pub struct KeyPair {
-    address: String,
-    private: String,
+    /// The count of transactions sent by a routine
+    #[clap(long, default_value_t = 20)]
+    count: u64,
+
+    /// load source accounts from file, or generate new accounts
+    #[clap(long)]
+    load: bool,
+
+    /// the source account file
+    #[clap(long, parse(from_os_str), value_name = "FILE", default_value = "source_keys.001")]
+    source: PathBuf,
+
+    /// block time of the network
+    #[clap(long, default_value_t = BLOCK_TIME)]
+    block_time: u64,
+
+    #[clap(subcommand)]
+    command: Option<Commands>,
 }
 
-fn one_eth_key() -> KeyPair {
-    let mnemonic = Mnemonic::generate_in(Language::English, Count::Words12);
-    let bs = mnemonic.to_seed("");
-    let ext = XPrv::derive_from_path(&bs, &DerivationPath::from_str("m/44'/60'/0'/0/0").unwrap()).unwrap();
-
-    let secret = SecretKey::parse_slice(&ext.to_bytes()).unwrap();
-    let public = PublicKey::from_secret_key(&secret);
-
-    let mut res = [0u8; 64];
-    res.copy_from_slice(&public.serialize()[1..65]);
-    let public = H160::from(H256::from_slice(Keccak256::digest(&res).as_slice()));
-
-    KeyPair {
-        address: eth_checksum::checksum(&format!("{:?}", public)),
-        private: hex::encode(secret.serialize()),
-    }
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct TransferMetrics {
-    from: Address,
-    to: Address,
-    amount: U256,
-    hash: Option<H256>, // Tx hash
-    status: u64,        // 1 - success, 0 - fail
-    wait: u64,          // seconds for waiting tx receipt
-}
-
-struct TestClient {
-    web3: Arc<web3::Web3<Http>>,
-    root_sk: secp256k1::SecretKey,
-    root_addr: Address,
-    rt: Runtime,
-}
-
-impl TestClient {
-    pub fn setup(url: Option<&str>, root_sk: Option<&str>, root_addr: Option<&str>) -> Self {
-        let transport = web3::transports::Http::new(url.unwrap_or(WEB3_SRV)).unwrap();
-        let web3 = Arc::new(web3::Web3::new(transport));
-        let root_sk = secp256k1::SecretKey::from_str(root_sk.unwrap_or(ROOT_SK)).unwrap();
-        let root_addr = Address::from_str(root_addr.unwrap_or(ROOT_ADDR)).unwrap();
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
-
-        Self {
-            web3,
-            root_sk,
-            root_addr,
-            rt,
-        }
-    }
-
-    pub fn chain_id(&self) -> Option<U256> {
-        self.rt.block_on(self.web3.eth().chain_id()).ok()
-    }
-
-    pub fn block_number(&self) -> Option<U64> {
-        self.rt.block_on(self.web3.eth().block_number()).ok()
-    }
-
-    pub fn gas_price(&self) -> Option<U256> {
-        self.rt.block_on(self.web3.eth().gas_price()).ok()
-    }
-
-    pub fn frc20_code(&self) -> Option<Bytes> {
-        self.rt
-            .block_on(self.web3.eth().code(H160::from_low_u64_be(FRC20_ADDRESS), None))
-            .ok()
-    }
-
-    #[allow(unused)]
-    pub fn transaction(&self, id: TransactionId) -> Option<Transaction> {
-        self.rt.block_on(self.web3.eth().transaction(id)).unwrap_or_default()
-    }
-
-    pub fn transaction_receipt(&self, hash: H256) -> Option<TransactionReceipt> {
-        self.rt
-            .block_on(self.web3.eth().transaction_receipt(hash))
-            .unwrap_or_default()
-    }
-
-    #[allow(unused)]
-    pub fn accounts(&self) -> Vec<Address> {
-        self.rt.block_on(self.web3.eth().accounts()).unwrap_or_default()
-    }
-
-    pub fn balance(&self, address: Address, number: Option<BlockNumber>) -> U256 {
-        self.rt
-            .block_on(self.web3.eth().balance(address, number))
-            .unwrap_or_default()
-    }
-
-    pub fn distribution(
-        &self,
-        source: Option<(secp256k1::SecretKey, Address)>,
-        accounts: &[&str],
-        amounts: &[U256],
-        block_time: Option<u64>,
-    ) -> web3::Result<(Vec<TransferMetrics>, u64)> {
-        let mut results = vec![];
-        let mut succeed = 0u64;
-        let mut idx = 1u64;
-        let total = accounts.len();
-        let source_address = source.unwrap_or((self.root_sk, self.root_addr)).1;
-        let source_sk = source.unwrap_or((self.root_sk, self.root_addr)).0;
-        let wait_time = block_time.unwrap_or(BLOCK_TIME) * 3 + 1;
-        accounts
-            .iter()
-            .zip(amounts)
-            .map(|(&account, &am)| {
-                let to = Some(Address::from_str(account).unwrap());
-                let tm = TransferMetrics {
-                    from: source_address,
-                    to: to.unwrap(),
-                    amount: am,
-                    ..Default::default()
-                };
-                let tp = TransactionParameters {
-                    to,
-                    value: am,
-                    ..Default::default()
-                };
-                (tp, tm)
-            })
-            // Sign the txs (can be done offline)
-            .for_each(|(tx_object, mut metric)| {
-                if let Ok(signed) = self
-                    .rt
-                    .block_on(self.web3.accounts().sign_transaction(tx_object, &source_sk))
-                {
-                    if let Ok(hash) = self
-                        .rt
-                        .block_on(self.web3.eth().send_raw_transaction(signed.raw_transaction))
-                    {
-                        metric.hash = Some(hash);
-                        let mut retry = wait_time;
-                        loop {
-                            if let Some(receipt) = self.transaction_receipt(hash) {
-                                if let Some(status) = receipt.status {
-                                    if status == U64::from(1u64) {
-                                        succeed += 1;
-                                        metric.status = 1;
-                                    }
-                                }
-                                metric.wait = wait_time + 1 - retry;
-                                break;
-                            } else {
-                                std::thread::sleep(Duration::from_secs(1));
-                                retry -= 1;
-                                if retry == 0 {
-                                    metric.wait = wait_time;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                println!("{}/{} {:?} {}", idx, total, metric.to, metric.status == 1);
-                idx += 1;
-                results.push(metric);
-            });
-
-        println!("Tx succeeded: {}/{}", succeed, total);
-
-        Ok((results, succeed))
-    }
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Fund Ethereum accounts
+    Fund {
+        /// the number of Eth Account to te fund
+        #[clap(long)]
+        count: u64,
+        /// fund amount
+        #[clap(long)]
+        amount: Option<u64>,
+    },
 }
 
 fn show_usage(prog: &str) {
@@ -211,13 +61,15 @@ fn show_usage(prog: &str) {
 }
 
 fn main() -> web3::Result<()> {
+    let _cli = Cli::parse();
+
     let mut per_count = 10;
     let mut source_count = 5;
     let mut prog = "feth".to_owned();
     let mut source_keys = None;
     let mut metrics = None;
     let mut block_time = None;
-    for (i, arg) in env::args().enumerate() {
+    for (i, arg) in std::env::args().enumerate() {
         if i == 0 {
             prog = arg;
         } else if i == 1 {
@@ -239,8 +91,8 @@ fn main() -> web3::Result<()> {
             block_time = Some(arg.parse::<u64>().unwrap_or(BLOCK_TIME));
         }
     }
-    let source_amount = U256::exp10(18 + 3); // 1000 eth
-    let target_amount = U256::exp10(17); // 0.1 eth
+    let source_amount = web3::types::U256::exp10(18 + 3); // 1000 eth
+    let target_amount = web3::types::U256::exp10(17); // 0.1 eth
 
     let client = TestClient::setup(None, None, None);
 
