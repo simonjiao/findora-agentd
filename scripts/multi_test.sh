4700e7f7da7e43655db3cfb2517a608050c08ad4
simonjiao <simon@findora.org>
2022-03-25 20:26:17 +0800

    update scripts

--- scripts/multi_test.sh
+++ scripts/multi_test.sh
@@ -8,27 +8,55 @@ port=26657
 script_dir=$(dirname "$0")
 . "$script_dir"/common.sh
 
-test_endpoint="http://dev-qa01-us-west-2-sentry-001-open.dev.findora.org:8545"
+test_endpoint="http://dev-qa01-us-west-2-full-000-open.dev.findora.org:8545"
 
 cd "$script_dir"/..
 
-#./feth --network "$test_endpoint" --count 2000 --max-parallelism 10 --timeout 100 >> test.log 2>&1
+#./feth --network "$test_endpoint" --count 1000 --max-parallelism 10 --timeout 100 >> test.log 2>&1
 #echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
 #sleep 60
 
-./feth --network "$test_endpoint" --count 2000 --max-parallelism 15 --timeout 100 >> test.log 2>&1
+#./feth --network "$test_endpoint" --count 1000 --max-parallelism 15 --timeout 100 >> test.log 2>&1
+#echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
+#sleep 60
+
+#./feth --network "$test_endpoint" --count 1000 --max-parallelism 20 --timeout 100 >> test.log 2>&1
+#echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
+#sleep 60
+#
+#./feth --network "$test_endpoint" --count 3000 --max-parallelism 25 --timeout 100 >> test.log 2>&1
+#echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
+#sleep 60
+#
+#./feth --network "$test_endpoint" --count 1000 --max-parallelism 30 --timeout 100 >> test.log 2>&1
+#echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
+#sleep 60
+#
+#./feth --network "$test_endpoint" --count 1000 --max-parallelism 35 --timeout 100 >> test.log 2>&1
+#echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
+#sleep 60
+#
+#./feth --network "$test_endpoint" --count 1000 --max-parallelism 40 --timeout 100 >> test.log 2>&1
+#echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
+#sleep 60
+
+./feth --network "$test_endpoint" --count 1000 --max-parallelism 45 --timeout 100 >> test.log 2>&1
 echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
 sleep 60
 
-./feth --network "$test_endpoint" --count 2000 --max-parallelism 20 --timeout 100 >> test.log 2>&1
+./feth --network "$test_endpoint" --count 1000 --max-parallelism 48 --timeout 100 >> test.log 2>&1
 echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
 sleep 60
 
-./feth --network "$test_endpoint" --count 2000 --max-parallelism 25 --timeout 100 >> test.log 2>&1
+./feth --network "$test_endpoint" --count 1000 --max-parallelism 50 --timeout 100 >> test.log 2>&1
 echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
 sleep 60
 
-./feth --network "$test_endpoint" --count 2000 --max-parallelism 30 --timeout 100 >> test.log 2>&1
+./feth --network "$test_endpoint" --count 1000 --max-parallelism 52 --timeout 100 >> test.log 2>&1
+echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
+sleep 60
+
+./feth --network "$test_endpoint" --count 1000 --max-parallelism 55 --timeout 100 >> test.log 2>&1
 echo "wait for 60 seconds to make sure all previous txns have been finished..." >> test.log
 sleep 60
 
