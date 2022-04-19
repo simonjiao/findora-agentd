1379577045e2aa6598d50d2b20593f7405d9ed05
simonjiao <simon@findora.org>
2022-04-19 17:19:27 +0800

    update scripts

--- scripts/multi_test.sh
+++ scripts/multi_test.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-test_endpoint="http://dev-qa01-us-west-2-full-001-open.dev.findora.org:8545"
+endpoint00="http://dev-qa01-us-west-2-full-000-open.dev.findora.org:8545"
+endpoint01="http://dev-qa01-us-west-2-full-001-open.dev.findora.org:8545"
 
 #cd "$script_dir"/..
 
@@ -12,16 +13,21 @@ run_one_test() {
     concurrency=$3
     timeout=$4
     waittime=$5
-    logfile=$6
+    source=$6
+    logfile=$7
 
     {
         echo "$endpoint $count $concurrency $timeout"
-        RUST_LOG=info ./feth --network "$endpoint" --count "$count" --max-parallelism "$concurrency" --timeout "$timeout" 2>&1
+        RUST_LOG=info ./feth --network "$endpoint" --source "$source" --count "$count" --max-parallelism "$concurrency" --timeout "$timeout" 2>&1
         echo "wait for 60 seconds to make sure all previous txns have been finished..."
     } >>"$logfile"
     sleep "$waittime"
 }
 
 for ((i = 0; i < 10000; i++)); do
-    run_one_test "$test_endpoint" 1 100 100 1 "test.log"
+    run_one_test "$endpoint00" 1 300 100 1 "source_keys.001" "test00.log"
+done &
+
+for ((i = 0; i < 10000; i++)); do
+    run_one_test "$endpoint01" 1 300 100 1 "source_keys.002" "test01.log"
 done
