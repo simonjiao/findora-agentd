1deb89f016b705c0db25d20aaadc98fe62c08a9a
simonjiao <simon@findora.org>
2022-04-19 16:59:25 +0800

    fix erros in scripts

--- scripts/multi_test.sh
+++ scripts/multi_test.sh
@@ -11,17 +11,17 @@ run_one_test() {
     count=$2
     concurrency=$3
     timeout=$4
-    wait=$6
-    logfile=$5
+    waittime=$5
+    logfile=$6
 
     {
         echo "$endpoint $count $concurrency $timeout"
         RUST_LOG=info ./feth --network "$endpoint" --count "$count" --max-parallelism "$concurrency" --timeout "$timeout" 2>&1
         echo "wait for 60 seconds to make sure all previous txns have been finished..."
     } >>"$logfile"
-    sleep "$wait"
+    sleep "$waittime"
 }
 
-for ((i = 0; i < 1000; i++)); do
-    run_one_test "$test_endpoint" 1 100 100 1 test.log
+for ((i = 0; i < 10000; i++)); do
+    run_one_test "$test_endpoint" 1 100 100 1 "test.log"
 done
