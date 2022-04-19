b1db9b87cac2e998fa201287862477b07b6f0a24
simonjiao <simon@findora.org>
2022-04-19 16:52:02 +0800

    update multi-test scripts

--- scripts/multi_test.sh
+++ scripts/multi_test.sh
@@ -7,26 +7,21 @@ test_endpoint="http://dev-qa01-us-west-2-full-001-open.dev.findora.org:8545"
 #cd "$script_dir"/..
 
 run_one_test() {
-  endpoint=$1
-  count=$2
-  concurrency=$3
-  timeout=$4
-  logfile=$5
+    endpoint=$1
+    count=$2
+    concurrency=$3
+    timeout=$4
+    wait=$6
+    logfile=$5
 
-  {
-    echo "$endpoint $count $concurrency $timeout"
-    RUST_LOG=info ./feth --network "$endpoint" --count "$count" --max-parallelism "$concurrency" --timeout "$timeout" 2>&1
-    echo "wait for 60 seconds to make sure all previous txns have been finished..."
-  } >>"$logfile"
-  sleep 60
+    {
+        echo "$endpoint $count $concurrency $timeout"
+        RUST_LOG=info ./feth --network "$endpoint" --count "$count" --max-parallelism "$concurrency" --timeout "$timeout" 2>&1
+        echo "wait for 60 seconds to make sure all previous txns have been finished..."
+    } >>"$logfile"
+    sleep "$wait"
 }
 
-run_one_test "$test_endpoint" 1000 45 100 test.log
-
-run_one_test "$test_endpoint" 1000 48 100 test.log
-
-run_one_test "$test_endpoint" 1000 50 100 test.log
-
-run_one_test "$test_endpoint" 1000 52 100 test.log
-
-run_one_test "$test_endpoint" 1000 55 100 test.log
+for ((i = 0; i < 1000; i++)); do
+    run_one_test "$test_endpoint" 1 100 100 1 test.log
+done
