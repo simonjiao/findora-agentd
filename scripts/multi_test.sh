ef3d63b38f8590e631d7db9bd887f7bd5be557a7
simonjiao <simon@findora.org>
2022-05-05 22:58:06 +0800

    update test scripts

--- scripts/multi_test.sh
+++ scripts/multi_test.sh
@@ -19,15 +19,39 @@ run_one_test() {
     {
         echo "$endpoint $count $concurrency $timeout"
         RUST_LOG=info ./feth --network "$endpoint" --source "$source" --count "$count" --max-parallelism "$concurrency" --timeout "$timeout" 2>&1
-        echo "wait for 60 seconds to make sure all previous txns have been finished..."
+        echo "wait for $waittime seconds to make sure all previous txns have been finished..."
     } >>"$logfile"
     sleep "$waittime"
 }
 
-for ((i = 0; i < 10000; i++)); do
-    run_one_test "$endpoint00" 1 300 100 1 "source_keys.001" "test00.log"
-done &
+switch_profiler() {
+    enable=$1
+    logfile=$2
+
+    if [ "$enable" ]; then
+        RUST_LOG=info ./feth profiler --network "$endpoint" --enable 2>&1 >>"$logfile"
+    else
+        RUST_LOG=info ./feth profiler --network "$endpoint" 2>&1 >>"$logfile"
+    fi
+
+}
+
+switch_profiler true "test01.log"
 
 for ((i = 0; i < 10000; i++)); do
-    run_one_test "$endpoint01" 1 300 100 1 "source_keys.002" "test01.log"
+    run_one_test "$endpoint01" 1 200 100 5 "source_keys.001" "test01.log"
 done
+
+switch_profiler false "test01.log"
+
+#for ((i = 0; i < 10000; i++)); do
+#    run_one_test "$endpoint01" 1 300 100 1 "source_keys.002" "test02.log"
+#done &
+#
+#for ((i = 0; i < 10000; i++)); do
+#    run_one_test "$endpoint00" 1 300 100 1 "source_keys.003" "test03.log"
+#done &
+#
+#for ((i = 0; i < 10000; i++)); do
+#    run_one_test "$endpoint01" 1 300 100 1 "source_keys.004" "test04.log"
+#done
